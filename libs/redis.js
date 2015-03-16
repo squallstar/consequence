9ba2acf436f21e21939679af25e9f957f516f2d2
Nicholas Valbusa <squallstar@gmail.com>
2015-03-16 02:03:28 +0000

    fix

--- libs/redis.js
+++ libs/redis.js
@@ -4,8 +4,8 @@ var client;
 if (process.env.REDISTOGO_URL) {
   var rtg = require('url').parse(process.env.REDISTOGO_URL),
       password = rtg.auth.split(':')[1],
-      pub = redis.createClient(rtg.port, rtg.host, { auth_pass: password }),
-      sub = redis.createClient(rtg.port, rtg.host, { detect_buffers: true, auth_pass: password});
+      pub = redis(rtg.port, rtg.host, { auth_pass: password }),
+      sub = redis(rtg.port, rtg.host, { detect_buffers: true, auth_pass: password});
 
   client = redis({ pubClient: pub, subClient: sub });
 } else {
